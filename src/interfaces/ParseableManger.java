37aea0039528ee7e4e8c6e611182b9a51708658c
원감 <wongam@kyonggi.ac.kr>
2024-04-02 17:09:14 +0900

    add : interface -basic of java

--- src/interfaces/ParseableManger.java
+++ src/interfaces/ParseableManger.java
@@ -16,7 +16,6 @@ public class ParseableManger {
     }
 }
 
-
 class XMLParser implements Parseable {
     public void parse(String fileName) {
         System.out.println(fileName + "-XML parsing completed");
